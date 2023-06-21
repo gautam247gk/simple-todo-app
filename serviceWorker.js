980dc5ef0857793cc65b2e09fb1943e64c2d1702
gautam247gk <gautam247gk@gmail.com>
2023-06-21 15:43:01 +0100

    Deploy to GitHub Pages

---
+++ serviceWorker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "v1";
+const CACHE_ASSETS = ["index.html", "offline.html"];
+
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(CACHE_ASSETS);
+    })
+  );
+});
+
+this.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
+this.addEventListener("activate", (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
